5b28a1ca2615e1819fb7b8b9e1c889bd1f68d2e9
elidsimo <elidrysymohamed2005@gmail.com>
2026-05-07 19:01:34 +0100

    stadyplanner backend

---
+++ backend/src/main/java/com/example/StadyplannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.stadyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StadyplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StadyplannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/stadyplanner/StadyplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stadyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadyplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
